fdcdee588de94f3d14b80c0ab2afc618378944ae
Shantanu Gontia <gontia.shantanu@gmail.com>
2024-12-31 17:50:05 -0800

    add rounding modes file

--- pkg/roundingmodes.go
+++ pkg/roundingmodes.go
@@ -1,14 +1,30 @@
 package floatBit
 
+import "math/big"
+
 // Package roundingModes provides constants (enum) for different rounding modes.
 // Rounding modes determine how to round a number when it cannot be represented exactly.
+
+type RoundingMode byte
 
 const (
 	// Round towards zero is equivalent to truncation
 	// Ties are broken by choosing the number closer to zero
-	RoundTowardsZero = 0
+	RTZ              RoundingMode = 0
+	RoundTowardsZero RoundingMode = 0
+
 	// Round Nearest Even breaks ties by choosing the number which is even.
 	// For rounding to non-integers, this means the least significant
 	// digit (in whichever radix) after rounding must be even
-	RoundNearestEven = 1
+	RNE              RoundingMode = 1
+	RoundNearestEven RoundingMode = 1
 )
+
+func (r RoundingMode) toBigRoundingMode() big.RoundingMode {
+	switch r {
+	case 0:
+		return big.ToZero
+	default:
+		return big.ToNearestEven
+	}
+}
