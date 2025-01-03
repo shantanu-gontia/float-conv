f7620e563d7aee59db566e9dc61b2718c1bc05f5
Shantanu Gontia <gontia.shantanu@gmail.com>
2025-01-03 08:37:05 -0800

    naming

--- pkg/float32/float32_test.go
+++ pkg/float32/float32_test.go
@@ -9,141 +9,141 @@ import (
 	outOfBounds "github.com/shantanu-gontia/float-conv/pkg/oob"
 )
 
-type InputVals struct {
+type inputVals struct {
 	input big.Float
 	r     floatBit.RoundingMode
 	o     outOfBounds.OverflowMode
 	u     outOfBounds.UnderflowMode
 }
 
-type OutputVals struct {
+type outputVals struct {
 	output Float32
 	acc    big.Accuracy
 	status outOfBounds.Status
 }
 
-type InputOutputPair struct {
-	ival InputVals
-	oval OutputVals
+type inputOutputPair struct {
+	ival inputVals
+	oval outputVals
 }
 
-type TestCase struct {
+type testCase struct {
 	name   string
-	ioVals []InputOutputPair
+	ioVals []inputOutputPair
 }
 
 func TestFromBigFloat(t *testing.T) {
-	testCases := []TestCase{
+	testCases := []testCase{
 		{
 			"PosInfInput",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				{
-					InputVals{*big.NewFloat(math.Inf(1)),
+					inputVals{*big.NewFloat(math.Inf(1)),
 						floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveInfinity}, big.Exact, outOfBounds.Fits},
+					outputVals{Float32{Float32PositiveInfinity}, big.Exact, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(math.Inf(1)), floatBit.RTZ, outOfBounds.MakeNaN, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveInfinity}, big.Exact, outOfBounds.Fits},
+					inputVals{*big.NewFloat(math.Inf(1)), floatBit.RTZ, outOfBounds.MakeNaN, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32PositiveInfinity}, big.Exact, outOfBounds.Fits},
 				},
 			},
 		},
 		{
 			"NegInfInput",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				{
-					InputVals{*big.NewFloat(math.Inf(-1)),
+					inputVals{*big.NewFloat(math.Inf(-1)),
 						floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32NegativeInfinity}, big.Exact, outOfBounds.Fits},
+					outputVals{Float32{Float32NegativeInfinity}, big.Exact, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(math.Inf(-1)), floatBit.RTZ, outOfBounds.MakeNaN, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32NegativeInfinity}, big.Exact, outOfBounds.Fits},
+					inputVals{*big.NewFloat(math.Inf(-1)), floatBit.RTZ, outOfBounds.MakeNaN, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32NegativeInfinity}, big.Exact, outOfBounds.Fits},
 				},
 			},
 		},
 		{
 			"PosZeroInput",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				{
-					InputVals{*big.NewFloat(0.0), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveZero}, big.Exact, outOfBounds.Fits},
+					inputVals{*big.NewFloat(0.0), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32PositiveZero}, big.Exact, outOfBounds.Fits},
 				},
 			},
 		},
 		{
 			"NegZeroInput",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				{
-					InputVals{*big.NewFloat(float64(math.Float32frombits(Float32NegativeZero))),
+					inputVals{*big.NewFloat(float64(math.Float32frombits(Float32NegativeZero))),
 						floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32NegativeZero}, big.Exact, outOfBounds.Fits},
+					outputVals{Float32{Float32NegativeZero}, big.Exact, outOfBounds.Fits},
 				},
 			},
 		},
 		{
 			"NormalNumbers",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				{
-					InputVals{*big.NewFloat(1.2323), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0x3f9dbc02}, big.Above, outOfBounds.Fits},
+					inputVals{*big.NewFloat(1.2323), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0x3f9dbc02}, big.Above, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(1.2323), floatBit.RTZ, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0x3f9dbc02}, big.Above, outOfBounds.Fits},
+					inputVals{*big.NewFloat(1.2323), floatBit.RTZ, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0x3f9dbc02}, big.Above, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(-0.1), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0xbdcccccd}, big.Below, outOfBounds.Fits},
+					inputVals{*big.NewFloat(-0.1), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0xbdcccccd}, big.Below, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(-0.1), floatBit.RTZ, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0xbdcccccd}, big.Below, outOfBounds.Fits},
+					inputVals{*big.NewFloat(-0.1), floatBit.RTZ, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0xbdcccccd}, big.Below, outOfBounds.Fits},
 				},
 				// Overflow
 				{
-					InputVals{*big.NewFloat(3.4028235e38), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveMaxNormal}, big.Below, outOfBounds.Overflow},
+					inputVals{*big.NewFloat(3.4028235e38), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32PositiveMaxNormal}, big.Below, outOfBounds.Overflow},
 				},
 				{
-					InputVals{*big.NewFloat(3.4028235e38), floatBit.RNE, outOfBounds.SaturateInf, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveInfinity}, big.Above, outOfBounds.Overflow},
+					inputVals{*big.NewFloat(3.4028235e38), floatBit.RNE, outOfBounds.SaturateInf, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32PositiveInfinity}, big.Above, outOfBounds.Overflow},
 				},
 				{
-					InputVals{*big.NewFloat(-3.4028235e38), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32NegativeMaxNormal}, big.Above, outOfBounds.Overflow},
+					inputVals{*big.NewFloat(-3.4028235e38), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32NegativeMaxNormal}, big.Above, outOfBounds.Overflow},
 				},
 				{
-					InputVals{*big.NewFloat(-3.4028235e38), floatBit.RNE, outOfBounds.SaturateInf, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32NegativeInfinity}, big.Below, outOfBounds.Overflow},
+					inputVals{*big.NewFloat(-3.4028235e38), floatBit.RNE, outOfBounds.SaturateInf, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32NegativeInfinity}, big.Below, outOfBounds.Overflow},
 				},
 			},
 		},
 		{
 			"SubnormalNumbers",
-			[]InputOutputPair{
+			[]inputOutputPair{
 				// Smallest Float32 Subnormal
 				{
-					InputVals{*big.NewFloat(float64(math.Float32frombits(0x0000_0001))), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0x0000_0001}, big.Exact, outOfBounds.Fits},
+					inputVals{*big.NewFloat(float64(math.Float32frombits(0x0000_0001))), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0x0000_0001}, big.Exact, outOfBounds.Fits},
 				},
 				// More precision than Float32 has
 				{
-					InputVals{*big.NewFloat(1.2288e-41), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0x0000_2241}, big.Below, outOfBounds.Fits},
+					inputVals{*big.NewFloat(1.2288e-41), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0x0000_2241}, big.Below, outOfBounds.Fits},
 				},
 				{
-					InputVals{*big.NewFloat(-1.2288e-41), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{0x8000_2241}, big.Above, outOfBounds.Fits},
+					inputVals{*big.NewFloat(-1.2288e-41), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{0x8000_2241}, big.Above, outOfBounds.Fits},
 				},
 				// Underflow
 				{
-					InputVals{*big.NewFloat(1e-46), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
-					OutputVals{Float32{Float32PositiveMinSubnormal}, big.Above, outOfBounds.Underflow},
+					inputVals{*big.NewFloat(1e-46), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.SaturateMin},
+					outputVals{Float32{Float32PositiveMinSubnormal}, big.Above, outOfBounds.Underflow},
 				},
 				{
-					InputVals{*big.NewFloat(1e-46), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.FlushToZero},
-					OutputVals{Float32{Float32PositiveZero}, big.Below, outOfBounds.Underflow},
+					inputVals{*big.NewFloat(1e-46), floatBit.RNE, outOfBounds.SaturateMax, outOfBounds.FlushToZero},
+					outputVals{Float32{Float32PositiveZero}, big.Below, outOfBounds.Underflow},
 				},
 			},
 		},
